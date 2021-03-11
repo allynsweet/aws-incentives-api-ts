80693fdfc86cae7ca51d2288bad406489950a595
allynsweet <allyn.sweet@uconn.edu>
2021-03-11 18:46:20 -0500

    reduce slack username length

--- src/common/helper.ts
+++ src/common/helper.ts
@@ -5,7 +5,7 @@ export async function sendError(errorMessage: string): Promise<void> {
     const SLACK_WEBHOOK =
         "https://hooks.slack.com/services/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";
     await axios.post(SLACK_WEBHOOK, {
-        username: `GridLite Error Handler - Reward Referral`,
+        username: `GridLite/referrals`,
         text: `:alert: ${errorMessage}`,
     });
     throw new Error(errorMessage);
